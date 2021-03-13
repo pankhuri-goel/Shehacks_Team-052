f05d52b89baedc50b68646bb4e4729a852affde3
Tulsi9 <72484847+Tulsi9@users.noreply.github.com>
2021-03-14 00:41:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)
+
 engine.setProperty('voice', voices[0].id)
 
 
@@ -17,7 +17,7 @@ def speak(audio):
     engine.runAndWait()
 
 
-def wishMe():
+def Greetings():
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
         speak("Good Morning!")
@@ -28,10 +28,10 @@ def wishMe():
     else:
         speak("Good Evening!")  
 
-    speak("I am Jarvis Sir. Please tell me how may I help you")       
+    speak(" Please tell me how may I help you")       
 
 def takeCommand():
-    #It takes microphone input from the user and returns string output
+    
 
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -45,7 +45,7 @@ def takeCommand():
         print("User said: ",query)
 
     except Exception as e:
-        # print(e)    
+         
         print("Say that again please...")  
         return "None"
     return query
@@ -59,12 +59,12 @@ def sendEmail(to, content):
     server.close()
 
 if __name__ == "__main__":
-    wishMe()
+    Greetings()
     while True:
-    # if 1:
+    
         query = takeCommand().lower()
 
-        # Logic for executing tasks based on query
+        
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
@@ -84,24 +84,26 @@ if __name__ == "__main__":
 
 
         elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            music_dir = 'D:\\xyz\\songs\\abc'#path of your music directory
             songs = os.listdir(music_dir)
-            print(songs)    
-            os.startfile(os.path.join(music_dir, songs[0]))
+            print(songs) 
+            speak('Which song number do you want me to play')
+            num = takeCommand()
+            os.startfile(os.path.join(music_dir, songs[num-1]))
 
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak("Sir, the time is ",strTime)
 
         elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            codePath = "C:\\Users\\abc\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"#target of the application
             os.startfile(codePath)
 
         elif 'email to harry' in query:
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "harryyourEmail@gmail.com"    
+                to = "ourEmail@gmail.com"    
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
